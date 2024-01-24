7b26bad48fbeb1390533485432d13e479eb14fd8
Nate Rollins <nater9ster@gmail.com>
2024-01-24 07:26:05 -0500

    moved index.html to top level

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./styles/kellytours.css"; // Main CSS file
+import App from './src/App'; // Main component
+import reportWebVitals from "./src/reportWebVitals";
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App /> {/* Render the main component here */}
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+
+reportWebVitals();
+
+
