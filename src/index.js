b8f19cd6e3852c1f1a8a45db8b9355d2fb1e31cf
Nate Rollins <nater9ster@gmail.com>
2024-01-26 11:42:27 -0500

    Moved index file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./styles/kellytours.css"; // Main CSS file
-import App from './src/App'; // Main component
-import reportWebVitals from "./src/reportWebVitals";
+import App from './App'; // Main component
+import reportWebVitals from "./reportWebVitals";
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
