247faf5b9871d04bfb811e3dcc7db61401ef8ef3
Nate Rollins <nater9ster@gmail.com>
2024-01-30 22:05:21 -0500

    Facebook web update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
     <div className="footer-container" align='center'>
       <h3 className="footer-text">KellyManhattanTours LLC</h3>
       <div className="social-icons">
-        <a href="https://www.facebook.com/kellymanhattan/" className="facebook social">
+        <a href="https://www.facebook.com/kkellymanhattan/" className="facebook social">
           <FontAwesomeIcon icon={faFacebook} size="2x" />
         </a>
         <a href="https://www.instagram.com/kellymanhattantours" className="instagram social">
